ff09147c05acb6e1c4886f6b20d6fcdbb35cbdac
faique <49121486+faiqu3@users.noreply.github.com>
2023-02-20 23:50:29 +0530

    Update secretfinder.sh

--- secretfinder.sh
+++ secretfinder.sh
@@ -2,5 +2,5 @@ domain=$(cat domains)
 for line in $domain; 
 do
   echo $line | waybackurls | grep -E "*.js" > urls
-  nuclei -silent -l urls -t ~/Documents/pentest/scripts/js-secret.yaml
+  nuclei -silent -l urls -t ~/Tools/js-secret.yaml
 done
